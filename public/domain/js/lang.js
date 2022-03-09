f0a7a2dd8ac2967d60e44ddc8e2d259e745d53b0
pedra <prbr@ymail.com>
2022-03-09 17:29:06 -0300

    adj

---
+++ public/domain/js/lang.js
@@ -0,0 +1,38 @@
+const config = {
+    lang: {
+        en: [
+            "For Sale!",
+            "Estimated value",
+            "by domain",
+            "Premium domain name available for instantly purchase. Invest in a premium domain name to launch your brand.",
+            "More Contacts",
+            "Make Your offer",
+            "Please fill out the form below so that the seller receive your offer.",
+            "Enter Your Name",
+            "Enter Your Email",
+            "Enter Your Offer",
+            "Please select one or more domains.",
+            "Send My Offer"
+        ],
+        pt: [
+            "À Venda!",
+            "Valor estimado",
+            "por domínio",
+            "Nome de domínio premium disponível para compra instantânea. Invista em um nome de domínio premium para lançar sua marca.",
+            "Mais Contatos",
+            "Faça Sua Oferta",
+            "Por favor, preencha o formulário abaixo para que o vendedor receba sua oferta.",
+            "Digite Seu Nome",
+            "Digite Seu Email",
+            "Faça Sua Oferta",
+            "Selecione um ou mais domínios.",
+            "Enviar Minha Oferta"
+        ],
+    },
+    actlang: 'en',
+    domains: [
+        "VotaBr.com",
+        "Urna22.com",
+        "VotaBrasil2022.com"
+    ]
+}
--- public/domain/js/main.js
+++ public/domain/js/main.js
@@ -1,5 +1,10 @@
 window.onload = () => {
 
+    _a('.lang img').forEach(a => a.onclick = changeLang)
+
+    renderLang()
+    setDomains()
+
     _('#contact-form').onsubmit = e => {
         e.preventDefault()
 
@@ -13,6 +18,28 @@ window.onload = () => {
         if (i > 0) _('#contact-form').submit()
     }
 
+    _('#container').classList.add('active')
 }
+const setDomains = () => {
+    _('#domains-check').innerHTML = ''
+    _('#domains-list').innerHTML = ''
+    config.domains.map((a, b) => {
+        _('#domains-check').innerHTML += `<div class="position-relative icon-group mb-3 in-line"><input id="d${b}" type="checkbox" class="form-control border-2" checked><label for="d${b}">${a}</label></div>`
+        _('#domains-list').innerHTML += `<h2 class="text-14 fw-700 text-white mb-4 domain-name">${a}</h2>`
+    })
+}
+
+const changeLang = e => {
+    config.actlang = e.target.id.replace('lang-', '')
+    _a('.lang img').forEach(a => a.classList.remove('active'))
+    e.target.classList.add('active')
+    renderLang()
+}
+
+const renderLang = () => _a('[data-lang]').forEach(a => {
+    d = a.dataset.lang -1
+    a[(d >= 7 && d <= 9) ? 'placeholder' : 'innerHTML'] = config.lang[config.actlang][d]
+})
+
 const _ = e => document.querySelector(e) || false
 const _a = e => document.querySelectorAll(e) || false
